d70a5aaefb14a95c4384e7fc82cefcb4ede18365
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2015-04-28 15:22:56 -0700

    add test for link class, add link class to satisfy test

--- llspecs.rb
+++ llspecs.rb
@@ -1,8 +1,19 @@
 require_relative 'linkedlist.rb'
+require_relative 'link.rb'
+
+describe "link" do
+  describe "#initialize" do
+    let(:my_link) {Link.new}
+
+    it "makes a new link" do
+      expect(my_link).to be_an_instance_of Link
+    end
+  end
+end
 
 describe "linked list" do
   describe "#initialize" do
-    let(:my_linked_list){LinkedList.new}
+    let(:my_linked_list) {LinkedList.new}
 
     it "makes a new linked list class" do
       expect(my_linked_list).to be_an_instance_of LinkedList
